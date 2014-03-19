d9b45cb6ce6ab6674a276e9369e0820c5202f09d
SupItsDillon <tmmdillon@gmail.com>
2014-03-19 14:46:49 +0000

    Update PlayerListener.java

--- me/SupItsDillon/DillFreedom/PlayerListener.java
+++ me/SupItsDillon/DillFreedom/PlayerListener.java
@@ -5,6 +5,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+//Plugin imports etc..
+import me.SupItsDillon.DillFreedom.DillFreedom
  
 public class PlayerListener implements Listener{
  
@@ -17,6 +20,7 @@ public class PlayerListener implements Listener{
         @EventHandler(priority = EventPriority.HIGHEST)
         public void onPlayerJoin(PlayerJoinEvent e){
                 e.getPlayer().sendMessage(ChatColor.RED+"This server is using DillFreedom!");
+                e.getPlayer().sendMessage(ChatColor.Red+ "To get the source go to github.com/SupItsDillon/DillFreedom");
         }
        
        
