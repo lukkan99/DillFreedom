11ff9c728b22d62e55d6df015546021403e363ee
lukkan99 <lukkan99@gmail.com>
2014-05-18 17:36:50 +0200

    move that back

--- me/SupItsDillon/DillFreedom/DillFreedom.java
+++ me/SupItsDillon/DillFreedom/DillFreedom.java
@@ -1,15 +1,16 @@
 package me.SupItsDillon.DillFreedom;
 
-import me.SupItsDillon.DillFreedom.Commands.Command_dffeed;
-import me.SupItsDillon.DillFreedom.Commands.Command_dfheal;
-import me.SupItsDillon.DillFreedom.Commands.Command_dfhelp;
-import me.SupItsDillon.DillFreedom.Commands.Command_dflol;
+
 import net.pravian.bukkitlib.BukkitLib;
 import net.pravian.bukkitlib.implementation.BukkitPlugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 //Command imports
+import me.SupItsDillon.DillFreedom.Commands.Command_dffeed;
+import me.SupItsDillon.DillFreedom.Commands.Command_dfheal;
+import me.SupItsDillon.DillFreedom.Commands.Command_dfhelp;
+import me.SupItsDillon.DillFreedom.Commands.Command_dflol;
 
 public class DillFreedom extends BukkitPlugin {
 
